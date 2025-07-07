2badf9e496c900dbc36f68d80d3eefec579c9a33
ArbindaRizki <arbindarm2005@gmail.com>
2025-07-07 12:18:47 +0700

    rapihin dashboard

--- app/src/main/java/com/example/uas/DashboardActivity.kt
+++ app/src/main/java/com/example/uas/DashboardActivity.kt
@@ -11,7 +11,7 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dashboard)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets

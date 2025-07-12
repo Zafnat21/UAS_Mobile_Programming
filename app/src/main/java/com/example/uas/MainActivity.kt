89c36e9fe97d729fd57ae4b793b0e4b023cd83c3
ArbindaRizki <arbindarm2005@gmail.com>
2025-07-12 21:40:26 +0700

    pakein gif

--- app/src/main/java/com/example/uas/MainActivity.kt
+++ app/src/main/java/com/example/uas/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
 
         val welcomeText = findViewById<TextView>(R.id.welcomeText)
         welcomeText.text = "Hi $username, Welcome"
+
+        val imageView = findViewById<ImageView>(R.id.meditatingImage)
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.meditating)
+            .into(imageView)
 
         val btnMulai = findViewById<Button>(R.id.btnMulai)
         btnMulai.setOnClickListener {
